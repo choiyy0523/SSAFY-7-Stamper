bd8b7644c38203b0ed46b4e8f4c1bae513f117ee
ohhyounkyu <ok_or@naver.com>
2022-09-23 12:41:06 +0900

    [FE] feat: 파비콘 추가, 폰트 추가, 로그인 페이지 추가(미완성), 기존 페이지 삭제 및 수정

---
+++ frontend/src/components/login/login.jsx
@@ -0,0 +1,39 @@
+import "./login.css"
+
+const login = () => {
+    return (
+      <div>
+      {/* 스탬퍼 로고와 이름 */}
+        <div>             
+          <img class="logo" src="/LOGO.png" alt="" />
+          <h2>Stamper</h2>
+          <h1>스탬퍼</h1>
+        </div>
+
+      {/* 로그인 관련  */}
+        <div>            
+          <input type="text" />
+            <br />
+          <input type="text" />
+            <br />
+          <input type="submit" />
+            <br />
+          <div class="between">
+            <a href="">회원가입</a>
+            <a href="">아이디/비밀번호 찾기</a>
+          </div>
+
+        </div>
+
+      {/* 하단 문구 */}
+        <p>스탬퍼가 되어 서울의 랜드마크를 수집해주세요!</p>
+        <p>곳곳에 있는 AI 친구들을 통해 기념도장을 모아보세요!</p>
+      
+      {/* 출처 표시 Footer */}
+        <a href="https://www.flaticon.com/authors/freepik" title="LOGO and Components icons">Logo and Components icons created by Freepik - Flaticon</a>
+      </div>
+
+    );
+  };
+  
+  export default login;
