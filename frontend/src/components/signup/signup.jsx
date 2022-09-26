b961596a7babd3c91d77b56fde30bc295d446708
최윤영 <adokids2@gmail.com>
2022-09-26 17:20:15 +0900

    [FE] for merge

--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,3 +1,18 @@
-export default function signup() {
-    return (<div>signup</div>)
+import "./signup.css"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@material-ui/core/TextField';
+import { Button } from "@mui/material";
+
+const signup = () => {
+    return (
+        <div>
+            {/* 회원가입 페이지 제목 + 이미지들 */}
+            <span>회원가입</span>
+
+
+        </div>
+    )
 }
+
+export default signup;
