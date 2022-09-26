de7007f895770c37d0ba78b16cf4b46776d3492b
이경준 <oct301@naver.com>
2022-09-26 11:56:52 +0900

    feat: Add Book Controller & Service

--- backend/src/main/java/com/ssafy/api/controller/BookController.java
+++ backend/src/main/java/com/ssafy/api/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.service.BookService;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
+
+    @Autowired
+    BookService bookService;
+
 
 }
---
+++ backend/src/main/java/com/ssafy/api/service/BookServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ssafy.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("bookService")
+public class BookServiceImpl implements BookService{
+}
