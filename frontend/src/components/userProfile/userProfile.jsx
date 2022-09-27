895404e5ddcf4a6628e14a46328807a33de7a55f
ohhyounkyu <ok_or@naver.com>
2022-09-27 20:34:00 +0900

    [FE] 유저 프로필 페이지 container, item 설정 중, 레이아웃 추가 조정 필요

--- frontend/src/components/userProfile/userProfile.jsx
+++ frontend/src/components/userProfile/userProfile.jsx
@@ -5,17 +5,17 @@ const userprofile = () => {
     return (
         <div>
             {/* 프로필 카드 */}
-            <div>
+            <div className="container profile_background">
                 {/* 프로필 사진과 이름 */}
-                <div>
-                    <section className="profile_photo">
+                <div className="item">
+                    
                         <img className="temp_dummy_profile_photo" src="https://via.placeholder.com/150" alt="" />
-                    </section>
+                    
                     <p>석호필</p>
                 </div>
                     
                 {/* 프로필의 기념도장과 랜드마크 아이콘 */}
-                <div>
+                <div className="item">
                     <img className="profile_comp" src="/src/assets/components/stamp.png" alt="" />
                         <p>10</p>
                         <p>기념도장(프로필 카드 안)</p>
@@ -25,20 +25,26 @@ const userprofile = () => {
                 </div>
 
                 {/* 자기소개 */}
-                <div>
+                <div className="item">
                     <p>나이: 29  성별: 남  취미: 클라이밍</p>
                     <p>성북구 랜드마크 뿌셔!</p>
                 </div>
 
-                <a href="/profile/:userNo/modify">
+                <a className="item" href="/profile/:userNo/modify">
                     <img className="profile_control_comp" src="/src/assets/components/write.png" alt="" />
                 </a>
             </div>
             {/* 프로필 카드 끝 */}
 
 
+            
+<br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br /><br /><br /><br /><br /><br />
             {/* 기념 도장 목록 (프로필 카드 밖) */}
             <div>
+            <h1>(레이아웃 조정 필요)</h1>
                 <h2>기념도장</h2>
                 <img className="profile_control_comp" src="/src/assets/components/stamp_tooltip.png" alt="" />
                 <li>유저별 모은 기념도장(업적)</li>
