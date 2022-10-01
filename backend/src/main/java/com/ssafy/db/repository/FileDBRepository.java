c2aadc35320de211e83c622428c5a65fe43ac796
이경준 <oct301@naver.com>
2022-10-01 15:28:24 +0900

    [BE] delete: FileDB deleted

--- backend/src/main/java/com/ssafy/db/repository/FileDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.FileDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileDBRepository extends JpaRepository<FileDB, String> {
-}
