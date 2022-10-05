4219dffdbc1ec1b985117c4306375acf6eed89d9
HongInho <inoh0914@gmail.com>
2022-10-05 16:36:54 +0900

    [FE] add: 지도 및 목록, 클릭

--- frontend/src/components/collectionSeoul/collectionSeoulDetail.jsx
+++ frontend/src/components/collectionSeoul/collectionSeoulDetail.jsx
@@ -4,14 +4,26 @@ import {useParams} from "react-router-dom";
 import { getListByGugun } from '../../api/book';
 import { Map, MapMarker, Polyline } from "react-kakao-maps-sdk";
 import { Fragment, useState, useRef} from 'react';
+import { useNavigate } from "react-router-dom";
 
 const collectionSeoulDetail = () =>{
 
-    const[fordata, setFordata] = useState({
-        arr : [],
+    // const navigate = useNavigate();
+    // const bookDetailLink = (bookseq) => {
+    //     //navigate(`/landmark/${bookseq}`);
+    //     console.log(bookseq);
+    // };
+
+    const[fordata, setFordata] = useState([]);
+    const[position, setPosition] = useState({
+        lat: 0, lng : 0
     });
 
     let bookCol = [];
+    let latAvg = 0;
+    let lngAvg = 0;
+    let latSum = 0
+    let lngSum = 0;
 
     let gugun = useParams().districtNo;
     useEffect(() => {
@@ -28,6 +40,8 @@ const collectionSeoulDetail = () =>{
             bookObj.bookSeq = response.data.totalBookList[i].bookSeq;
             bookObj.bookAddress = response.data.totalBookList[i].bookAddress;
             bookObj.collected = "미수집";
+            latSum += response.data.totalBookList[i].bookLatitude;
+            lngSum += response.data.totalBookList[i].bookLongitude;
 
             for (var j=0; j<response.data.collectedBookList.length; j++){
                 if (response.data.totalBookList[i].bookSeq == response.data.collectedBookList[j].book.bookSeq){
@@ -42,7 +56,13 @@ const collectionSeoulDetail = () =>{
 
         setFordata(bookCol);
  
-        console.log(fordata);
+        //console.log(fordata);
+
+        latAvg = latSum / response.data.totalBookList.length;
+        lngAvg = lngSum / response.data.totalBookList.length;
+
+
+        setPosition({lat : latAvg, lng : lngAvg});
 
 
         }, (error)=>{
@@ -54,16 +74,17 @@ const collectionSeoulDetail = () =>{
     return (<div>{useParams().districtNo} 수집 현황
         <div>
             <Map
-            center={{ lat: 33.5563, lng: 126.79581 }}
+            center={{ lat: position.lat, lng: position.lng}}
             style={{ width: "100%", height: "360px" }}
+            level={7}
             >
-            <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-                <div style={{color:"#000"}}>Hello World!</div>
-            </MapMarker>
-
-            <MapMarker position={{ lat: 33.55635, lng: 127 }}>
-                <div style={{color:"#000"}}>no!</div>
-            </MapMarker>
+                    {fordata.map((item)=>{
+                        return (
+                            <MapMarker position={{ lat: item.bookLatitude, lng: item.bookLongitude }}>
+                                <div style={{color:"#000"}}>{item.bookName}</div>
+                            </MapMarker>
+                        );
+                    })}
             </Map>
         </div>
 
@@ -78,10 +99,10 @@ const collectionSeoulDetail = () =>{
                     </tr>
                 </thead>
                 <tbody>
-                    {bookCol.map((item)=>{
+                    {fordata.map((item)=>{
                         return (
                             <tr>
-                            <td>{item.bookName}</td>
+                            <td><a href = {`/landmark/${item.bookSeq}`}>{item.bookName}</a></td>
                             <td>{item.bookAddress}</td>
                             <td>{item.collected}</td>
                             </tr>
