f8dfa94d58b88fd6329a26768f764d7eaaa77a56
Chan <saorin12@naver.com>
2022-10-05 20:21:52 +0900

    [FE] feat : S3 이미지 불러오기, 댓글 불러오기

--- frontend/src/components/landmark/landmark.jsx
+++ frontend/src/components/landmark/landmark.jsx
@@ -1,23 +1,67 @@
 import { useParams } from 'react-router-dom';
 import TextField from "@mui/material/TextField";
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { getBookDetail } from '../../api/book';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getCommentList } from '../../api/comment';
 
 export default function landmark() {
-    const landmarkNo = useParams();
-    console.log(landmarkNo['landmarkNo'])
+    const landmarkNo = useParams().landmarkNo;
+    // console.log(landmarkNo['landmarkNo'])
+
+    const [imageURL, setImageURL] = useState("");
+    const [landmarkName, setLandmarkName] = useState("");
+    const [landmarkDesc, setLandmarkDesc] = useState("");
+    const [landmarkComment, setLandmarkComment] = useState([]);
+
+    const baseURL = "https://stamperimage.s3.ap-northeast-2.amazonaws.com/defaultimg/";
 
     // const queryClient = useQueryClient()
     // const query = useQuery()
+
+    const userInfo = useSelector((state) => state.UserInfo);
+    const userName = userInfo.userInfo.userName;
+    const userNick = userInfo.userInfo.userNickname;
+    const userSeq = userInfo.userInfo.userSeq;
+    const token = userInfo.accessToken;
+
+    useEffect(() => {
+        getBookDetail(userSeq, landmarkNo, (response) => {
+            const data = response.data;
+
+            setLandmarkName(data.book.bookName);
+            setLandmarkDesc(data.book.bookDescription);
+
+
+            let tempURL = baseURL;
+            if (data.userBook) {
+                // 수집하지 않은 도감의 경우 기본이미지로 url 저장
+                tempURL += data.book.bookImageURL;
+                setImageURL(tempURL);
+            } else {
+                tempURL += data.userBook.userbookCollectionImage;
+                setImageURL(tempURL);
+            }
+        });
+        getCommentList(landmarkNo, token, (response) => {
+            const data = response.data;
+
+            setLandmarkComment(data.list);
+        })
+    }, [])
     
 
     return (
     <div>
-        <div>landmarkName</div>
+            <div>{landmarkName}</div>
         <div>
-            <img src='src\assets\approved.png' alt=''></img>
+            <img src={imageURL} alt={landmarkName}></img>
         </div>
         <div>
-            <p>대충 설명</p>
+                <p>대충 설명</p>
+                <p>{landmarkDesc}</p>
         </div>
         <button>수집</button>
         <div>
@@ -26,10 +70,29 @@ export default function landmark() {
         </div>
         <div>
             <aside>
-                <img src='src\assets\LOGO2.png'></img>
-            </aside>
-            <div>댓글제목</div>
-            <div>댓글내용</div>
+                <img src='../../src/assets/LOGO2.png' alt="LOGO2" width="30" height="30"></img>
+                </aside>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>작성자</th>
+                            <th>내용</th>
+                            <th>작성일자</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {landmarkComment.map((item) => {
+                            return (
+                                <tr>
+                                    <td>{item.user.userNickname}</td>
+                                    <td>{item.commentContent}</td>
+                                    <td>{item.commentDate}</td>
+                                </tr>
+                            );
+                        })}
+
+                    </tbody>
+                </table>
         </div>
         <button>더보기</button>
     </div>
